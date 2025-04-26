df53da60b6fa1408f2fc26dbc492ad0f80af19fa
Eduardo220898 <eduardozollner@gmail.com>
2025-04-26 13:56:19 -0300

    Alteração nos operadores

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+    if (min >= max) {
+        alert('O valor minimo tem que ser MENOR  que o valor maximo')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+    }
+
+
+
 
 }
 
